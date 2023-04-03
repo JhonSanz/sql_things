c6a04044877abecf6f026f2048369aef2f9f5ac4
JhonSanz <ingjhonsanz@utp.edu.co>
2023-04-02 19:51:00 -0500

    Added db generator files

---
+++ test_db_generator/init_database.sql
@@ -0,0 +1,38 @@
+CREATE TABLE countries (
+	country_id serial PRIMARY KEY,
+	country_name VARCHAR (255) UNIQUE NOT NULL
+);
+
+CREATE TABLE cities (
+	city_id serial PRIMARY KEY,
+	city_name VARCHAR (255) UNIQUE NOT NULL,
+	country_id INT NOT NULL,
+	FOREIGN KEY (country_id)
+		REFERENCES countries (country_id)
+);
+
+CREATE TABLE stores (
+	store_id serial PRIMARY KEY,
+	store_name VARCHAR (50) UNIQUE NOT NULL,
+	website VARCHAR (255) UNIQUE NOT NULL
+);
+
+CREATE TABLE products (
+	product_id serial PRIMARY KEY,
+	product_name VARCHAR (255) NOT NULL,
+	description VARCHAR (255) NOT NULL,
+	store_id INT NOT NULL,
+  	FOREIGN KEY (store_id)
+		REFERENCES stores (store_id)
+);
+
+CREATE TABLE store_city (
+	store_id INT NOT NULL,
+	city_id INT NOT NULL,
+	FOREIGN KEY (store_id)
+		REFERENCES stores (store_id),
+	FOREIGN KEY (city_id)
+		REFERENCES cities (city_id)
+);
+
+
---
+++ test_db_generator/populate_database.sql
@@ -0,0 +1,242 @@
+DELETE FROM public.products;
+DELETE FROM public.store_city;
+DELETE FROM public.stores;
+DELETE FROM public.cities;
+DELETE FROM public.countries;
+
+
+/* countries  */
+INSERT INTO public.countries(country_name) VALUES ('Colombia');
+INSERT INTO public.countries(country_name) VALUES ('Venezuela');
+INSERT INTO public.countries(country_name) VALUES ('Chile');
+INSERT INTO public.countries(country_name) VALUES ('Mexico');
+INSERT INTO public.countries(country_name) VALUES ('Peru');
+INSERT INTO public.countries(country_name) VALUES ('Argentina');
+
+
+/* cities  */
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Pereira', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Colombia')
+);
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Cartago', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Colombia')
+);
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Manizales', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Colombia')
+);
+
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Caracas', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Venezuela')
+);
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Maracaibo', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Venezuela')
+);
+
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Santiago', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Chile')
+);
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Valparaiso', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Chile')
+);
+
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Durango', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Mexico')
+);
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'DF', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Mexico')
+);
+
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Lima', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Peru')
+);
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Kuzco', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Peru')
+);
+
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Buenos Aires', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Argentina')
+);
+INSERT INTO public.cities(city_name, country_id) VALUES (
+	'Bernal', 
+	(SELECT country_id FROM public.countries WHERE country_name = 'Argentina')
+);
+
+
+/* stores  */
+INSERT INTO public.stores(store_name, website) VALUES ('Parkmotos', 'www.parkmotos.com');
+INSERT INTO public.stores(store_name, website) VALUES ('El pavo', 'www.elpavo.com');
+INSERT INTO public.stores(store_name, website) VALUES ('BBC', 'www.bbc.com');
+INSERT INTO public.stores(store_name, website) VALUES ('Chorilago', 'www.chorilago.com');
+
+
+/* store_city  */
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Pereira')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Santiago')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Kuzco')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'DF')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Maracaibo')
+);
+
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Manizales')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Cartago')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Durango')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Lima')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Valparaiso')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Caracas')
+);
+
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'DF')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Buenos Aires')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Pereira')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Lima')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Santiago')
+);
+
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Chorilago'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Bernal')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Chorilago'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Caracas')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Chorilago'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Kuzco')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Chorilago'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'DF')
+);
+INSERT INTO public.store_city(store_id, city_id) VALUES (
+	(SELECT store_id FROM public.stores WHERE store_name = 'Chorilago'), 
+	(SELECT city_id FROM public.cities WHERE city_name = 'Santiago')
+);
+
+/* products  */
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Gusanos espaciales', '4 unidades',
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Brownies', '500g',
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Grinder', 'Tamaño personal',
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Pipas', 'Monoplonicas',
+	(SELECT store_id FROM public.stores WHERE store_name = 'Parkmotos')
+);
+
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Cerveza', 'Poker, Costeña, Aguila',
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Aguardiente Antioqueño', '',
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Ron viejo de Caldas', 'Mejor que el Medellin',
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Aguardiente Amarillo', 'De manzana',
+	(SELECT store_id FROM public.stores WHERE store_name = 'El pavo')
+);
+
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Chapinero', 'Porter',
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Monserrate', 'Biter',
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Cajica', 'Honey Ale',
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Macondo', 'Stout',
+	(SELECT store_id FROM public.stores WHERE store_name = 'BBC')
+);
+
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Chorizo', 'De procedencia desconocida',
+	(SELECT store_id FROM public.stores WHERE store_name = 'Chorilago')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Chuzo', 'De animal desconocido',
+	(SELECT store_id FROM public.stores WHERE store_name = 'Chorilago')
+);
+INSERT INTO public.products(product_name, description, store_id) VALUES (
+	'Pizza', 'Rica',
+	(SELECT store_id FROM public.stores WHERE store_name = 'Chorilago')
+);
+
+
+
+
+
+
